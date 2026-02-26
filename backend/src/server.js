eb6a09947d09b955a61f34e6df6812f880940b5a
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-02-27 01:27:43 +0800

    Building NackEnd Authentication

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,9 @@ import { ENV } from './libs/env.js'
 import connectDB from './libs/db.js'
 import rateLimiter from './middleware/rateLimiter.js'
 
+import authRoutes from './routes/authRoutes.js'
+import dashboardRoutes from './routes/dashboardRoutes.js'
+
 const app = express();
 
 app.use(
@@ -18,6 +21,10 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(rateLimiter)
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/dashboard', dashboardRoutes);
 
 connectDB().then( () => {
     app.listen( ENV.PORT, () => {
