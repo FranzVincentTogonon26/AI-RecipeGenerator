b298a8316a433c4c8be5bce27250065c6f730e6c
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-03-06 16:31:57 +0800

    Roolback Code

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,6 @@ import express from 'express'
 import cors from 'cors'
 
 import { ENV } from './config/env.js'
-import { connectDB } from './config/db.js';
-
 import rateLimiter from './middleware/rateLimiter.js'
 
 import authRoutes from './routes/authRoutes.js'
@@ -35,20 +33,6 @@ app.use('/api/recipes', recipeRoutes);
 app.use('/api/meal-plans', mealPlanRoutes);
 app.use('/api/shopping-list', shoppingRoutes);
 
-/* -------------------- START SERVER -------------------- */
-
-const startServer = async () => {
-  try {
-    await connectDB();
-
-    app.listen(ENV.PORT, () => {
-      console.log(`Server running on port ${ENV.PORT}`);
-    });
-
-  } catch (error) {
-    console.error('Server failed to start');
-    process.exit(1);
-  }
-};
-
-startServer();
+app.listen( ENV.PORT, () => {
+    console.log(`Server running on port ${ENV.PORT}`)
+} )
